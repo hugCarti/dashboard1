debc3931cd5ab87f233e7dc164da29cb8b0a7548
HugCarti <Hleplingard04@gmail.com>
2025-09-04 10:21:38 +0200

    header created

---
+++ src/app/components/header.tsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <header>
+      <img src="public/vercel.svg" alt="logo" />
+      <ul>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </header>
+  );
+}
